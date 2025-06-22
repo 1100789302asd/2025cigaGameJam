81b0e76135a15038e805a19deef08168297ebc9e
superpai <1358275285@qq.com>
2025-06-22 21:04:28 +0800

    一些准备工作

---
+++ Assets/Scripts/InstanceBase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceBase<T> : MonoBehaviour where T:class
+{
+    public static T instacne;
+    void Awake()
+    {
+        if (instacne == null)
+            instacne = this as T;
+        else
+            print("error");
+    }
+}
